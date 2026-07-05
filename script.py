3cbd9f96c9f6a1f3efa816eced78b6f3766c9b15
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-05 10:23:39 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -3,9 +3,9 @@ import copy
 print("\033c\033[47;31m\ngive me file .class: ? \n")
 a=input().strip()
 rrr=a.replace(".class","")
-ttt="javap -c -private $1 > /tmp/$2.jbat".replace("$1",a).replace("$2",rrr)
+ttt="/usr/bin/openjdk-asmtools-jdis $1 -w /tmp/".replace("$1",a)
 os.system(ttt)
-f1=open("/tmp/$2.jbat".replace("$2",rrr),"r")
+f1=open("/tmp/$2.jasm".replace("$2",rrr),"r")
 bodys=f1.read()
 f1.close()
 
@@ -15,17 +15,14 @@ aa=False
 cc=False
 for bb in b:
     if line==1:
-        ll=bb.find("Compiled from")
-        if ll<0:
-            print("not a jbat file...")
-            exit(1)
-        else:
-            print(bb)
-            aa=True
+        
+        
+            
+        aa=True
     else:
         if cc:
             print(bb)
-            hh=bb.find(": return")
+            hh=bb.find("return")
             if hh>-1:
                 print("end program")
                 exit(0)
